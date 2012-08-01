ec381fa5850c4a13708a53ddc4490284e5536595
Luca <lucabel@gmail.com>
2012-08-01 16:47:22 +0200

    add translation to interface attributes

--- collective/limitfilesizepanel/interfaces.py
+++ collective/limitfilesizepanel/interfaces.py
@@ -2,23 +2,22 @@
 
 from zope.interface import Interface
 from zope import schema
-from Products.CMFPlone import PloneMessageFactory as pmf
 from collective.limitfilesizepanel import messageFactory as _
+
 
 class ILimitFileSizePanel(Interface):
     """
     Settings used in the control panel
     """
-    
+
     file_size = schema.Int(
-        title = pmf(u"Set the file-type size limit"),
-        description = pmf(u"Type here a number in Mb which will limit the file size upload"),
-        default = 30,
+        title=_(u"Set the file-type size limit"),
+        description=_(u"Type here a number in Mb which will limit the file size upload"),
+        default=30,
     )
-    
+
     image_size = schema.Int(
-        title=pmf(u"Set the image-type size limit"),
-        description=pmf(u"Type here a number in Mb which will limit the image size upload"),
-        default = 10,                      
+        title=_(u"Set the image-type size limit"),
+        description=_(u"Type here a number in Mb which will limit the image size upload"),
+        default=10,
     )
-    
