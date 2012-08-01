e8aee63ebf4bff89b13e875dc62173e564f2e01f
Luca <lucabel@gmail.com>
2012-08-01 16:47:55 +0200

    reorder import in order to avoid broken import form other module

--- collective/limitfilesizepanel/__init__.py
+++ collective/limitfilesizepanel/__init__.py
@@ -4,6 +4,8 @@ import logging
 from zope.i18nmessageid import MessageFactory
 
 project_name = 'collective.limitfilesizepanel'
-
 messageFactory = MessageFactory(project_name)
+
+from collective.limitfilesizepanel import monkeypatch
+
 logger = logging.getLogger(project_name)
